7a412fcba2e1502842a8179c37c05054b13cd318
yash15072008 <75427466+yash15072008@users.noreply.github.com>
2021-04-09 19:52:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
-var ball;
+var ball,database,position;
 
 function setup() {
-
+    database = firebase.database();
     createCanvas(600,600);
 
     ball = createSprite(300,300,50,50);
+    ball.shapeColor= "red";
+
+    var ballPosition = database.ref('ball/position');
+    ballPosition.on("value", readPosition, showError);
 
 }
 
@@ -14,20 +18,42 @@ function draw() {
 
     if(keyDown("left_arrow")){
 
-        ball.x = ball.x -3;
+       writePosition(-3,0);
     }
     if(keyDown("right_arrow")){
 
-        ball.x = ball.x +3;
+        writePosition(3,0);
     }
     if(keyDown("down_arrow")){
 
-        ball.y = ball.y +3;
+        writePosition(0,3); 
     }
     if(keyDown("up_arrow")){
 
-        ball.y = ball.y -3;
+        writePosition(0,-3);
     }
 
     drawSprites();
+}
+
+function writePosition(x,y) {
+
+    database.ref('ball/position').set({
+
+        'x': position.x + x,
+        'y': position.y + y
+    })
+}
+
+function readPosition(data) {
+
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+
+}
+
+function showError() {
+
+    console.log("error");
 }
